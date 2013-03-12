df415e4ac5370b83b66fe065bd50d4bc9619f4bf
trevhunter <trevhunter@gmail.com>
2013-03-11 23:53:17 -0400

    Entity Pattern
    Added the entity pattern to the library.

---
+++ src/duhmain.UnitTests/EntityTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace duhmain.UnitTests
+{
+    [TestFixture]
+    public class EntityTests
+    {
+        [Test]
+        [ExpectedException(ExpectedException=typeof(ArgumentNullException))]
+        public void Throws_when_null_id_passed()
+        {
+            var subject = new TestEntity(null);
+
+        }
+
+        [Test]
+        [ExpectedException(ExpectedException = typeof(ArgumentNullException))]
+        public void Throws_when_empty_id_passed()
+        {
+            var subject = new TestEntity(String.Empty);
+
+        }
+
+        [Test]
+        public void Returns_Set_Id()
+        {
+            string expected = "This is an expected value" + DateTime.Now.Ticks.ToString(); // Add random value at the end so test can't be easily faked.
+            var subject = new TestEntity(expected);
+
+            Assert.AreEqual(expected, subject.Id);
+        }
+
+
+
+        #region test helpers
+
+        /// <summary>
+        /// Mock entity
+        /// </summary>
+        private class TestEntity : Entity
+        {
+            public TestEntity()
+                :base()
+            {
+
+            }
+
+            public TestEntity(string id)
+                :base(id)
+            {
+
+            }
+
+        }
+
+        #endregion
+
+    }
+}
---
+++ src/duhmain/Entity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace duhmain
+{
+    /// <summary>
+    /// Represents an object that has an identity that is unique within a given context.
+    /// </summary>
+    public interface IEntity
+    {
+        /// <summary>
+        /// The unique identifier for this entity.
+        /// </summary>
+        /// <remarks>If this is null or empty, it likely means that the entity state is new and hasn't been committed to its owning repository.</remarks>
+        string Id { get; }
+    }
+
+
+    /// <summary>
+    /// Base implementation of an entity.
+    /// </summary>
+    public abstract class Entity : IEntity
+    {
+        #region constructore
+
+        public Entity()
+        {
+        }
+
+        public Entity(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                throw new ArgumentNullException("id", "The entity id cannot be null or empty.");
+            }
+            this.Id = id;
+        }
+
+        #endregion
+
+
+        /// <summary>
+        /// The unique identifier for this entity.
+        /// </summary>
+        /// <remarks>If this is null or empty, it likely means that the entity state 
+        /// is new and hasn't been committed to its owning repository.</remarks>
+        public string Id { get; private set; }
+
+    }
+}
