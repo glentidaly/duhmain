4e4582607b254523d0dfd4990f60036320b41e29
trevhunter <trevhunter@gmail.com>
2013-03-12 00:16:53 -0400

    Moved versioned entity into a file of its own
    Moved the versioned entity out of the entity file into a file of its own
    for clarity.

--- src/duhmain/Entity.cs
+++ src/duhmain/Entity.cs
@@ -18,18 +18,6 @@ namespace duhmain
         string Id { get; }
     }
 
-    /// <summary>
-    /// Represents an entity that contains an opaque version identifier (like an etag)
-    /// </summary>
-    public interface IVersionedEntity : IEntity 
-    {
-        /// <summary>
-        /// An opaque identifier for the version of this entity that may be 
-        /// used for optimistic concurrenty purposes.
-        /// </summary>
-        string Version { get; }
-
-    }
 
 
     /// <summary>
@@ -64,31 +52,5 @@ namespace duhmain
 
     }
 
-    /// <summary>
-    /// Base implementation of an entity that retains an opaque version identifier
-    /// that may be used for optimistic concurrency purposes.
-    /// </summary>
-    public abstract class VersionedEntity : Entity, IVersionedEntity
-    {
-        #region constructors
-
-        public VersionedEntity()
-        {
-        }
-
-        public VersionedEntity(string id, string version = null)
-            :base(id)
-        {
-            this.Version = version;
-        }
-
-        #endregion
-
-        /// <summary>
-        /// An opaque identifier for the version of this entity that may be 
-        /// used for optimistic concurrenty purposes.
-        /// </summary>
-        public string Version { get; private set; }
-
-    }
+  
 }
---
+++ src/duhmain/VersionedEntity.cs
@@ -0,0 +1,45 @@
+﻿
+namespace duhmain
+{
+    /// <summary>
+    /// Represents an entity that contains an opaque version identifier (like an etag)
+    /// </summary>
+    public interface IVersionedEntity : IEntity
+    {
+        /// <summary>
+        /// An opaque identifier for the version of this entity that may be 
+        /// used for optimistic concurrenty purposes.
+        /// </summary>
+        string Version { get; }
+
+    }
+
+    /// <summary>
+    /// Base implementation of an entity that retains an opaque version identifier
+    /// that may be used for optimistic concurrency purposes.
+    /// </summary>
+    public abstract class VersionedEntity : Entity, IVersionedEntity
+    {
+        #region constructors
+
+        public VersionedEntity()
+        {
+        }
+
+        public VersionedEntity(string id, string version = null)
+            : base(id)
+        {
+            this.Version = version;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// An opaque identifier for the version of this entity that may be 
+        /// used for optimistic concurrenty purposes.
+        /// </summary>
+        public string Version { get; private set; }
+
+    }
+
+}
